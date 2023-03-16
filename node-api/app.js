5a18af5189de73b0e9268d731b12f25f8b4e9643
Joaquim Codina <joaquim.codina.espin@gmail.com>
2023-03-16 20:46:01 +0100

    realitzar proves amb base de dades firebase, obtenir documents de collection i mostrar-ho en format json

--- node-api/app.js
+++ node-api/app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { db } = require('./routes/firebase');
 
 var app = express();
 
@@ -19,8 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+app.get('/', async (req, res) => {
+  const querySnapshot = await db.collection('cities').get()
+  res.json(querySnapshot.docs.map(doc => doc.data()));
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
