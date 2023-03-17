8faf93dfa3134f15abb88fcef79a73728c5e0a5b
Joaquim Codina <joaquim.codina.espin@gmail.com>
2023-03-18 00:44:51 +0100

    Mostrar detalls recepta a partir del seu ID

--- node-api/routes/index.js
+++ node-api/routes/index.js
@@ -3,9 +3,27 @@ const { db } = require('./firebase');
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/recipes', async (req, res) => {
   const querySnapshot = await db.collection("recipes").get();
   res.json(querySnapshot.docs.map(doc => doc.data()));
+});
+
+router.get('/recipes/:recipeId', async (req, res) => {
+  try{
+    const docId = req.params.recipeId;
+    const docRef = db.collection("recipes").doc(docId);
+    const doc = await docRef.get();
+
+    if(!doc.exists){
+      return res.status(404).send('No ha trobat el document');
+    }
+    return res.json(doc.data());
+    
+  } 
+  catch(error){
+    console.error(error);
+    return res.status(500).send('Error al obtenir els detalls');
+  }
 });
 
 module.exports = router;
