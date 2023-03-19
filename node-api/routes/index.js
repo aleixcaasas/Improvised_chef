41a1fec8111706c78df087ffd5dad738f6066046
Joaquim Codina <joaquim.codina.espin@gmail.com>
2023-03-19 17:33:50 +0100

    Ruta per cercar receptes a partir dels ingredients que disposa el usuari

--- node-api/routes/index.js
+++ node-api/routes/index.js
@@ -9,6 +9,32 @@ router.get('/recipes', async (req, res) => {
   res.json(querySnapshot.docs.map(doc => doc.data()));
 });
 
+router.get('/recipes/:userIngredientsList', async (req, res) => {
+  try{
+    var results = [];
+    const docId = req.params.userIngredientsList;
+    const recipes = await db.collection("recipes").get();
+    const user = await db.collection("users").doc(docId).get();
+    if(!user.exists){
+      return res.status(404).send('No ha trobat el document');
+    }
+    recipes.forEach((doc) => {
+      user.data().ingredients.forEach((ingredientUser) => {
+        doc.data().ingredients.forEach((ingredientRecipe) => {
+          if(ingredientRecipe.toLowerCase().includes(ingredientUser.toLowerCase())){
+            results.push(doc.data());
+          }
+        })
+      })
+    })
+    return res.json([...new Map(results.map(v => [v.title, v])).values()]);
+  } 
+  catch(error){
+    console.error(error);
+    return res.status(500).send('Error al obtenir les dades');
+  }
+})
+
 router.get('/recipes/:recipeId', async (req, res) => {
   try{
     const docId = req.params.recipeId;
