05abb8d4581dcae76b2fe124a70e58810a36ca9e
joaquimcodina <joaquim.codina.espin@gmail.com>
2023-03-26 12:46:35 +0200

    Inicialitzar paquets CORS i BODY-PARSER en app.js

--- node-api/app.js
+++ node-api/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mockRouter = require('./mock/mock');
@@ -16,6 +18,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', indexRouter);
