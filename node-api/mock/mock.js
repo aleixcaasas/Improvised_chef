6546b534d90b3e0fc1e04328d1e8a89900c3d4e9
joaquimcodina <joaquim.codina.espin@gmail.com>
2023-03-30 00:53:36 +0200

    Crear nous endpoints, mostrar ingredients, cercar ingredients per nom, cercar receptes per ingredients

--- node-api/mock/mock.js
+++ node-api/mock/mock.js
@@ -2,21 +2,41 @@ const { Router } = require('express');
 const fs = require('fs');
 const router = Router();
 const recipes = [];
+const ingredients = [];
+const dataRecipes = '../data/parsed_recipes.json';
+const dataIngredients = '../data/parsed_ingredients.json';
 
-fs.readFile('./data/parsed_recipes.json', 'utf8', (err, data) => {
+fs.readFile(dataRecipes, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return('Error al llegir arxiu JSON');
     }
     const obj = JSON.parse(data);
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 4; i++) {
         const recipe = obj[i];
         let ingredients = [];
         for (let j = 0 ; j < recipe.ingredients.length ; j++){
             const ingredient = recipe.ingredients[j];
             ingredients.push({ id: ingredient.id, name: ingredient.name, quantity: ingredient.quantity, unit: ingredient.unit});
         }
-        recipes.push({ title: recipe.title, image: recipe.image, ingredients: ingredients});
+        recipes.push({ title: recipe.title, image: recipe.image, ingredients: ingredients, id: recipe.id});
+    }
+});
+
+fs.readFile(dataIngredients, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return('Error al llegir arxiu JSON');
+    }
+    const obj = JSON.parse(data);
+    for (let i = 0; i < 4; i++) {
+        const ingredient = obj[i];
+        let recipesIngredient = [];
+        for (let j = 0 ; j < ingredient.recipes_in.length ; j++){
+            const recipe = ingredient.recipes_in[j];
+            recipesIngredient.push(recipe);
+        }
+        ingredients.push({ name: ingredient.name, id: ingredient.id, recipes_in: recipesIngredient});
     }
 });
 
@@ -32,12 +52,23 @@ router.get('/recipes/ingredients', async (req, res) => {
     let recipesIngredients = [];
     recipes.forEach(recipe => {
         recipe.ingredients.forEach(ingredient => {
-            if(req.body.ingredients.trim().split(',').includes(ingredient.name.toLowerCase())){
+            if(ingredients.some(obj => obj.id === ingredient.id)){
                 recipesIngredients.push(recipe);
             }
         })
     })
-    return res.json(recipesIngredients);
+    const arraySinDuplicados = recipesIngredients.filter((obj, index, self) =>
+            !index || !self.slice(0, index).some((o) => o.id === obj.id)
+    );
+    return res.json(arraySinDuplicados);
+});
+
+router.get('/ingredients', async (req, res) => {
+    return res.json(ingredients);
+});
+
+router.post('/ingredients/name', async (req, res) => {
+    return res.json(ingredients.filter(obj => obj.name.toString().toLowerCase().includes(req.body.name.toLowerCase())));
 });
 
 module.exports = router;
