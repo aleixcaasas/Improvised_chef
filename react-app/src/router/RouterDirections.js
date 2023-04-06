9cb1cf10c87e6809457220589c841ec9bb071d20
Guillemtejonestalella <101114041+Guillemtejonestalella@users.noreply.github.com>
2023-04-06 17:02:09 +0200

    message error login/register v1

--- react-app/src/router/RouterDirections.js
+++ react-app/src/router/RouterDirections.js
@@ -5,10 +5,11 @@ import Home from '../pages/Home';
 import UserAuthC from '../components/login/UserAuthC';
 import ForgotPassword from '../components/login/ForgotPassword';
 import MockComponent from '../components/mock/MockComponent';
-import SearchBar from '../components/searchBar/SearchBar';
+import SearchBar from '../components/SearchBar/SearchBar';
 import Components from '../pages/Components'
-import SideBar from '../components/sideBar/SideBar'
+import SideBar from '../components/sideBar/sideBar'
 import ResumeRecipe from '../components/resume_recipe/Resume_recipe';
+import ErrorLoginRegister from '../components/Error_login_register/ErrorLoginRegister';
 
 export default function RouterDirections() {
   return (
@@ -23,6 +24,7 @@ export default function RouterDirections() {
         <Route exact path="/components/sidebar" element={<SideBar></SideBar>} />
         <Route exact path="/components/searchbar" element={<SearchBar></SearchBar>} />
         <Route exact path="/components/resume_recipe" element={<ResumeRecipe/>} />
+        <Route exact path="/components/Error_login_register" element={<ErrorLoginRegister></ErrorLoginRegister>} />
 
         <Route path='*' element={<UserAuthC><Home /></UserAuthC>} />
       </Routes>
