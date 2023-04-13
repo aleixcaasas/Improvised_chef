6d3c470e52dbfb64870b63d13e6afb836cac6eaa
aleixcaasas <acasasbarco@gmail.com>
2023-04-13 20:31:25 +0200

    SideBar links done

--- react-app/src/components/sideBar/SideBar.js
+++ react-app/src/components/sideBar/SideBar.js
@@ -6,8 +6,6 @@ import { auth } from "../../firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
 import './sideBar.css';
 
-
-
 const Sidebar = () => {
     const [user, setUser] = useState({});
     //const {logOut} = useAuth();
@@ -22,10 +20,10 @@ const Sidebar = () => {
             <div className="sidebar-top">
                 <div className="user-div">
                     <div className="image-div">
-                        <img className="user-image" src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg"></img>
+                        <Link to="/Profile"><img className="user-image" src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg"></img></Link>
                     </div>
-                    <h3 className="name">JUAN COCINERO</h3>
-                    <a className="username">@juancocinero</a>
+                    <Link to="/Profile"><h3 className="name">JUAN COCINERO</h3></Link>
+                    <Link to="/Profile"><label className="username">@juancocinero</label></Link>
                 </div>
                 <div className="logout-div">
                     <button /*onClick={handleLogOut}*/><MdLogout size={22}></MdLogout> <label>Sign Out</label></button>
@@ -33,22 +31,22 @@ const Sidebar = () => {
             </div>
             <div className="sidebar-bottom">
                 <ul>
-                    <Link to=""><li><MdOutlineHome size={30} /><label>Home</label></li></Link>
+                    <Link to="/demo"><li><MdOutlineHome size={30} /><label>Home</label></li></Link>
 
 
-                    <Link to=""><li><MdPersonOutline size={30} /><label>Profile</label></li></Link>
+                    <Link to="/Profile"><li><MdPersonOutline size={30} /><label>Profile</label></li></Link>
 
 
-                    <Link to=""><li><MdOutlineKitchen size={30} /><label>My Kitchen</label></li></Link>
+                    <Link to="/MyKitchen"><li><MdOutlineKitchen size={30} /><label>My Kitchen</label></li></Link>
 
 
-                    <Link to=""><li><TbCheese size={30} /><label>My Ingredients</label></li></Link>
+                    <Link to="/MyIngredients"><li><TbCheese size={30} /><label>My Ingredients</label></li></Link>
 
 
-                    <Link to=""><li><MdOutlineShoppingCart size={30} /><label>Shopping List</label></li></Link>
+                    <Link to="/ShoppingList"><li><MdOutlineShoppingCart size={30} /><label>Shopping List</label></li></Link>
 
 
-                    <Link to=""><li><MdOutlineFavoriteBorder size={30} /><label>My Favourites</label></li></Link>
+                    <Link to="/FavouriteRecipes"><li><MdOutlineFavoriteBorder size={30} /><label>My Favourites</label></li></Link>
                 </ul>
             </div>
 
