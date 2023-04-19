6f5a00111df613a2001ee6c2034a80c24a255b9e
Èric <113594121+ericsubirana@users.noreply.github.com>
2023-04-19 13:32:21 +0200

    missatges d'errror en el ForgotPassword

--- react-app/src/components/login/ForgotPassword.js
+++ react-app/src/components/login/ForgotPassword.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import axios from 'axios';
+import ErrorMessage from '../errorMessages/ErrorMessage';
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ForgotPassword() {
 
+    const navigation = useNavigate();
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
+	const [error2, setError2] = useState({error:false, comment:""});
+
+	const clicked = (message) => {
+        if(message === "true"){
+            navigation("/home");
+        }
+    };
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -14,21 +24,24 @@ export default function ForgotPassword() {
 
 	if (message === "") {
 		return (
-			<div className="page_login">
-				<div className="container" id="forgot_password">
-					<h1>Reset your password</h1>
-					<form className="login-form" onSubmit={handleSubmit}>
-						<input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"></input>
-						<button type="submit">Send email</button>
-					</form>
-				</div>
+			<div className="container" id="forgot_password">
+
+				<h1>Reset your password</h1>
+				<form className="login-form" onSubmit={handleSubmit}>
+					<input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"></input>
+					<button type="submit">Send email</button>
+				</form>
 			</div>
 		);
 	} else if (message === "Password reset email send correctly") {
 		return (
-			<h3>{message}</h3>
+			navigation("/home")
 		);
 	} else {
-		return <h3>{message}</h3>;
+		return(
+			<div className="pep">
+					<ErrorMessage errorMessage={message} clicked={clicked}></ErrorMessage>;
+			</div>		
+		)	
 	}
 }
