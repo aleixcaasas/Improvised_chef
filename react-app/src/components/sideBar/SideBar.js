76090c472a5c9aec84756402df060f5984a7e595
PolLopezPena <pollopezpena@gmail.com>
2023-04-21 18:34:18 +0200

    desplegable responsive

--- react-app/src/components/sideBar/SideBar.js
+++ react-app/src/components/sideBar/SideBar.js
@@ -48,14 +48,8 @@ const Sidebar = () => {
                 <FaBars onClick={showSidebar}></FaBars>
             </Link>
         </div>
-        <div className={sidebar ? 'sidebar active' : 'sidebar'}>
-            <div className="closeIconDiv">
-                <Link to='#' className="closeIcon">
-                    <AiOutlineClose onClick={showSidebar}></AiOutlineClose>
-                </Link>
-            </div>
+        <div className='sidebar'>
             <div className="sidebar-top">
-                
                 <div className="user-div">
                     {response?.data && (
                         <>
@@ -97,6 +91,58 @@ const Sidebar = () => {
                     <Link to="/FavouriteRecipes"><li><MdOutlineFavoriteBorder size={30} /><label>My Favourites</label></li></Link>
                 </ul>
             </div>
+            
+        </div>
+        <div className={sidebar ? 'desplegable' : 'desplegable active'}>
+            <div className="closeIconDiv">
+                <Link to='#' className="closeIcon">
+                    <AiOutlineClose onClick={showSidebar}></AiOutlineClose>
+                </Link>
+            </div>
+            <div className="sidebar-top">
+                
+                <div className="user-div">
+                    {response?.data && (
+                        <>
+                            <div className="image-div">
+                                <Link to="/Profile"><img className="user-image" src={response.data[0].profilePic}></img></Link>
+                            </div>
+                            <Link to="/Profile"><h3 className="name">{response.data[0].fullName}</h3></Link>
+                            <Link to="/Profile"><label className="username">@{response.data[0].userName}</label></Link>
+                        </>                          
+                    )}
+                    {!response?.data && (
+                        <>
+                            <h1>Loading...</h1>
+                        </>                       
+                    )}
+                    
+                </div>
+                <div className="logout-div">
+                    <button onClick={handleLogOut}><MdLogout size={22}></MdLogout> <label>Sign Out</label></button>
+                </div>
+            </div>
+            <div className="sidebar-bottom">
+                <ul>
+                    <Link to="/home"><li><MdOutlineHome size={30} /><label>UwU</label></li></Link>
+
+
+                    <Link to="/Profile"><li><MdPersonOutline size={30} /><label>Profile</label></li></Link>
+
+
+                    <Link to="/MyKitchen"><li><MdOutlineKitchen size={30} /><label>My Kitchen</label></li></Link>
+
+
+                    <Link to="/MyIngredients"><li><TbCheese size={30} /><label>My Ingredients</label></li></Link>
+
+
+                    <Link to="/ShoppingList"><li><MdOutlineShoppingCart size={30} /><label>Shopping List</label></li></Link>
+
+
+                    <Link to="/FavouriteRecipes"><li><MdOutlineFavoriteBorder size={30} /><label>My Favourites</label></li></Link>
+                </ul>
+            </div>
+            
         </div>
         </>
     )
