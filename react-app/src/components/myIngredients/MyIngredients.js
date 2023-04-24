b90c1dc89ba394ed71b1c489f54bd15e08ac06f9
Guillemtejonestalella <101114041+Guillemtejonestalella@users.noreply.github.com>
2023-04-24 12:37:39 +0200

    Avanç del MyIngredients list
    actualitzacio del component

--- react-app/src/components/myIngredients/MyIngredients.js
+++ react-app/src/components/myIngredients/MyIngredients.js
@@ -1,3 +1,20 @@
-export default function MyIngredients() {
 
+
+import './MyIngredients.css';
+
+export default function MyIngredients() {
+    return(
+        <div className="div-myIngridients">
+            <div className="div-outBox">   
+                <h2 className='ingredientsTitle'>INGREDIENTS LIST</h2>             
+                    <div className="div-inBox">
+                        <ul>
+                            <li>Elemento 1</li>
+                            <li>Elemento 2</li>
+                            <li>Elemento 3</li>
+                        </ul>
+                    </div>
+            </div>
+        </div>
+    );
 }
