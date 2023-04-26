b74e96bca0fdf4d9b9dbc233e1df6d09df8c0c3d
PolLopezPena <pollopezpena@gmail.com>
2023-04-26 18:44:51 +0200

    1

--- react-app/src/components/favoriteRecipes/FavoriteRecipes.js
+++ react-app/src/components/favoriteRecipes/FavoriteRecipes.js
@@ -1,5 +1,17 @@
-
-
+import recipes from "./Test_receipt.json";
+import "./FavoriteRecipes.css";
 export default function FavoriteRecipes() {
+    const name = recipes.
+    return (
+        <>
+            <div className="container">
+                <p>My favourite recipes</p>
+                <div className="list-container">
+                    <li>
 
+                    </li>
+                </div>
+            </div>
+        </>
+    )    
 }
