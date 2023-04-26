d63d6e5e4cbba6e15f47c86c395272f09b8b4afe
Guillemtejonestalella <101114041+Guillemtejonestalella@users.noreply.github.com>
2023-04-26 10:23:45 +0200

    avanç myIngredients

--- react-app/src/components/myIngredients/MyIngredients.js
+++ react-app/src/components/myIngredients/MyIngredients.js
@@ -5,8 +5,9 @@ import './MyIngredients.css';
 export default function MyIngredients() {
     return(
         <div className="div-myIngridients">
-            <div className="div-outBox">   
-                <h2 className='ingredientsTitle'>INGREDIENTS LIST</h2>             
+            <div className="div-outBox">                   
+                <h2 className="ingredientsTitle">INGREDIENTS LIST</h2>          
+                <button className="addButton">Add more ingredients to the list</button>   
                     <div className="div-inBox">
                         <ul>
                             <li>Elemento 1</li>
