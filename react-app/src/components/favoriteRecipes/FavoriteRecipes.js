1c70f6a82a35724dd447ef8f3f79cbf26a2b92d9
PolLopezPena <pollopezpena@gmail.com>
2023-04-28 10:39:25 +0200

    primeros intentos

--- react-app/src/components/favoriteRecipes/FavoriteRecipes.js
+++ react-app/src/components/favoriteRecipes/FavoriteRecipes.js
@@ -1,15 +1,14 @@
 import recipes from "./Test_receipt.json";
 import "./FavoriteRecipes.css";
 export default function FavoriteRecipes() {
-    const name = recipes.title;
     return (
         <>
             <div className="container">
                 <p>My favourite recipes</p>
                 <div className="list-container">
-                    <li>
-                        {name}
-                    </li>
+                    <ul>
+                        {recipes.map((dict) => <li>{dict.title}</li>)}
+                    </ul>
                 </div>
             </div>
         </>
