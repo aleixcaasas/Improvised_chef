509d418b4080a6b96650a6c4e10ca3868a9788d6
aleixcaasas <acasasbarco@gmail.com>
2023-05-05 16:25:25 +0200

    Updated firebase-config file with alternative db

--- node-api/firebase/firebase-config.js
+++ node-api/firebase/firebase-config.js
@@ -14,6 +14,13 @@ const firebaseConfig = {
   messagingSenderId: "785865687041",
   appId: "1:785865687041:web:1bedf6742cec76f2de4000",
   measurementId: "G-YPK6ERRE9X"
+  /*apiKey: "AIzaSyCremjjBCYR7iA0wz-xl3MplYOq1ADzZ9E",
+  authDomain: "prova-bd-ae518.firebaseapp.com",
+  projectId: "prova-bd-ae518",
+  storageBucket: "prova-bd-ae518.appspot.com",
+  messagingSenderId: "780344726761",
+  appId: "1:780344726761:web:a3db3603ca02a04c1c0c78",
+  measurementId: "G-W9Q8ZNFBZ1"*/
 };
 
 // Initialize Firebase
