5a610d8e32a3dd0e7e635fb2789803a5164f701b
aleixcaasas <acasasbarco@gmail.com>
2023-05-05 17:23:24 +0200

    Shopping list page created

--- react-app/src/components/shoppingList/ShoppingList.js
+++ react-app/src/components/shoppingList/ShoppingList.js
@@ -1,7 +1,52 @@
+
+import ingredients from './ingredients.json';
+import './ShoppingList.css';
+import SearchIngredient from '../searchIngredient/SearchIngredient';
+import { useState } from 'react';
+import classNames from 'classnames';
+import { GiFruitBowl } from 'react-icons/gi';
+import { BsTrash } from 'react-icons/bs';
+
 export default function ShoppingList() {
-    return(
-        <div>
-            
+    const [call, setCall] = useState({ clicked: false })
+
+    const clicked = async (message) => {
+        if (message === 'true') {
+            setCall({ clicked: false })
+        }
+        else {
+            setCall({ clicked: true })
+        }
+    }
+
+    const myIngredientsClass = classNames('div-myIngridients', { 'dark': call.clicked });
+    const outBox = classNames('div-outBox', { 'dark': call.clicked });
+
+    return (
+        <div className={myIngredientsClass}>
+            <div className={outBox}>
+                <h2 className="ingredientsTitle">SHOPPING LIST</h2>
+                <button className="addButton" onClick={() => {clicked('false') }}>Add more ingredients to the list</button>
+                <div className="div-inBox">
+                    <ul>
+                        {ingredients.map((ingredient) => (
+
+                            <li><GiFruitBowl></GiFruitBowl>
+                                <label>{ingredient.name}</label>
+                                <BsTrash></BsTrash>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            {call.clicked && (
+                <div>
+                    <div className='popUpIngredients'>
+                        <SearchIngredient clicked={clicked}></SearchIngredient>
+                    </div>
+                </div>
+
+            )}
         </div>
-    )
+    );
 }
--- react-app/src/pages/ShoppingList.js
+++ react-app/src/pages/ShoppingList.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Sidebar from "../components/sideBar/SideBar";
+import ShoppingList from "../components/shoppingList/ShoppingList";
 
-export default function ShoppingList() {
+export default function ShoppingListPage() {
     return (
         <div className="page">
-            AQUESTA SERÀ LA PÀGINA DE LA LLISTA DE LA COMPRA DE L'USUARI
-            ES MOSTRARÀ A L'ESQUERRA EL SIDEBAR I TOTA LA PART DRETA DE LA PANTALLA HI HAURÀ 1 LLISTA GRAN AMB
-            TOTS ELS INGREDIENTS I UN BOTÓ DE + PER A AFEGIR INGREDIENTS A LA LLISTA
-            S'HAURÀ DE FER UN COMPONENT QUE ES VEURÀ BE LA LLISTA DE LA COMPRA I UN ALTRE COMPONENT PER AL BOTÓ DE +:
+            <Sidebar/>
+            <ShoppingList/>
         </div>
     );
 
