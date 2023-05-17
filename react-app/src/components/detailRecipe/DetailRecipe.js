472cf083ef3dec6831a77d380115bd200ab065b2
Guillemtejonestalella <101114041+Guillemtejonestalella@users.noreply.github.com>
2023-05-17 12:37:36 +0200

    icono favourites afegit
    llibreria Md

--- react-app/src/components/detailRecipe/DetailRecipe.js
+++ react-app/src/components/detailRecipe/DetailRecipe.js
@@ -1,10 +1,12 @@
 import "./DetailRecipe.css"
+import { MdStars } from 'react-icons/md';
 
 export default function DetailRecipe() {
     return (
         <div className="div-Recipe">
             <div className="general-div">
                 <div className="detail-title-div">
+                    <MdStars size={40} className="fav-icon"/>
                     <h2 className="detail-title">NOM RECEPTA</h2>
                     <div className="prepareButt-div"><button className="prepareButt" >Prepare / add</button></div>
                 </div>
