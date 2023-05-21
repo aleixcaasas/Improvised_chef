60202997c7b1797c07e3907e414ef98b74393ff8
Èric <113594121+ericsubirana@users.noreply.github.com>
2023-05-21 16:55:51 +0200

    sideBar arreglada

--- react-app/src/pages/Recipe.js
+++ react-app/src/pages/Recipe.js
@@ -1,10 +1,11 @@
 import Sidebar from "../components/sideBar/SideBar";
 import { useLocation } from 'react-router-dom';
 import DetailRecipe from '../components/detailRecipe/DetailRecipe'
+import React, { useState } from 'react';
 
 export default function Recipe() {
     const location = useLocation();
-    const { infoRecipe } = location.state;
+    const [infoRecipe, setInfoRecipe] = useState(location.state ? location.state.infoRecipe : null);
 
     return (
         <div className="page">
