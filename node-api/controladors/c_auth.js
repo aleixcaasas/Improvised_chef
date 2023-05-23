715150e363047c5f8511458d4abbcc41a29a93ac
Èric <113594121+ericsubirana@users.noreply.github.com>
2023-05-23 08:04:32 +0200

    eliminarUsuariBD/Authentication

--- node-api/controladors/c_auth.js
+++ node-api/controladors/c_auth.js
@@ -1,19 +1,19 @@
-const {signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, sendPasswordResetEmail, getAuth} = require('firebase/auth');
-const {auth, db, getDoc} = require('../firebase/firebase-config');
-const {addDoc, collection, getDocs, query, setDoc, doc, deleteDoc} = require('firebase/firestore');
+const { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, sendPasswordResetEmail, getAuth } = require('firebase/auth');
+const { auth, db, getDoc } = require('../firebase/firebase-config');
+const { addDoc, collection, getDocs, query, setDoc, doc, deleteDoc } = require('firebase/firestore');
 const profilePic = 'https://img.freepik.com/free-icon/user_318-563642.jpg';
 
 const loginWithEmail = async function (email, password) {
     let result;
     try {
         result = await signInWithEmailAndPassword(auth, email, password);
-        if(result.user.uid){ //comprovar
+        if (result.user.uid) { //comprovar
             return {
                 loguejat: true, id: result.user.uid
             };
         }
-        else{
-            return {loguejat: false, id: null};
+        else {
+            return { loguejat: false, id: null };
         }
     }
     catch (error) {
@@ -30,9 +30,9 @@ const emailNotExists = async (email) => {
     let emailExist = false;
 
     querySnapshot.forEach((doc) => {
-      if(doc.data().email === email){
-        emailExist = true;
-      }
+        if (doc.data().email === email) {
+            emailExist = true;
+        }
     });
     return emailExist; //modificar consulta i fer-la directe desde un select
 }
@@ -54,22 +54,24 @@ const loginWithGoogle = async (result) => {
             let shoppingList = [];
             let favoriteRecipes = [];
             userRef = await setDoc(doc(db, "users", result.uid), {
-            fullName,
-            userName,
-            profilePic,
-            email,
-            userId: `${result.uid}`,
-            myIngredients,
-            shoppingList,
-            favoriteRecipes
-          });
+                fullName,
+                userName,
+                profilePic,
+                email,
+                userId: `${result.uid}`,
+                myIngredients,
+                shoppingList,
+                favoriteRecipes
+            });
         }
 
-        return {loguejat: true,
-                email : email,
-                id: `${result.uid}`};
+        return {
+            loguejat: true,
+            email: email,
+            id: `${result.uid}`
+        };
     } catch (error) {
-        return {loguejat: false};
+        return { loguejat: false };
     }
 };
 
@@ -79,10 +81,10 @@ const signOutV = async function (email, password) {
 
 const registerWithEmail = async (fullName, userName, email, password) => {
     let id = null;
-    try{
+    try {
         await createUserWithEmailAndPassword(auth, email, password).then(
             async (result) => {
-                try{
+                try {
                     let myIngredients = [];
                     let shoppingList = [];
                     let favoriteRecipes = [];
@@ -97,44 +99,49 @@ const registerWithEmail = async (fullName, userName, email, password) => {
                         favoriteRecipes
                     });
                     id = result.user.uid;
-                } catch(error){
+                } catch (error) {
                     console.error("ERROR adding doc", error);
                 }
             }
         )
     }
-    catch(e){
+    catch (e) {
         return false;
     }
-    return {id: id, loguejat: true};
+    return { id: id, loguejat: true };
 };
 
 const resetPasswordEmail = async (email) => {
-    try{
-        if (await emailNotExists(email)){ //email exists
-            await sendPasswordResetEmail(auth,email);
+    try {
+        if (await emailNotExists(email)) { //email exists
+            await sendPasswordResetEmail(auth, email);
             return "Password reset email send correctly";
         }
-    }catch (error) {
+    } catch (error) {
         return String(error);
     }
     return "There is not any account with this email";
 }
 
 const deleteUser = async (userId) => {
-  try {
-    const userDocRef = doc(db, "users", userId);
-    const userDocSnap = await getDoc(userDocRef);
-    if (userDocSnap.exists()) {
-      await deleteDoc(userDocRef);
-      return [200, "Account deleted successfully!"];
-    } else {
-      return [404, "There is not any account with this ID"];
+    try {
+        const userDocRef = doc(db, "users", userId);
+        const userDocSnap = await getDoc(userDocRef);
+        if (userDocSnap.exists()) {
+            const auth = getAuth();
+            const user = auth.currentUser;
+            if (user) {
+                user.delete();
+            } 
+            await deleteDoc(userDocRef);
+            return [200, "Account deleted successfully!"];
+        } else {
+            return [404, "There is not any account with this ID"];
+        }
+    } catch (error) {
+        return [500, String(error)];
     }
-  } catch (error) {
-    return [500, String(error)];
-  }
 };
 
 
-module.exports = {registerWithEmail, signOutV, loginWithGoogle, loginWithEmail, resetPasswordEmail, deleteUser};
+module.exports = { registerWithEmail, signOutV, loginWithGoogle, loginWithEmail, resetPasswordEmail, deleteUser };
