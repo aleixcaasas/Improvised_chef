fe2b9e629567ae99183aee27211c4ead6c91fcb2
Aleix Casas Barco <70470419+aleixcaasas@users.noreply.github.com>
2023-06-29 23:39:27 +0200

    Update firebase-config.js

--- react-app/src/firebase-config.js
+++ react-app/src/firebase-config.js
@@ -7,22 +7,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	/*
-	apiKey: "AIzaSyCpVw3wUH3_NWm4WUF1cLIe1_eCeZCEMGI",
-	authDomain: "cheff-improvisado.firebaseapp.com",
-	projectId: "cheff-improvisado",
-	storageBucket: "cheff-improvisado.appspot.com",
-	messagingSenderId: "785865687041",
-	appId: "1:785865687041:web:1bedf6742cec76f2de4000",
-	measurementId: "G-YPK6ERRE9X"
-	*/
-	apiKey: "AIzaSyCremjjBCYR7iA0wz-xl3MplYOq1ADzZ9E",
-	authDomain: "prova-bd-ae518.firebaseapp.com",
-	projectId: "prova-bd-ae518",
-	storageBucket: "prova-bd-ae518.appspot.com",
-	messagingSenderId: "780344726761",
-	appId: "1:780344726761:web:a3db3603ca02a04c1c0c78",
-	measurementId: "G-W9Q8ZNFBZ1"
+	apiKey: "--------------------------------------",
+	authDomain: "--------------.firebaseapp.com",
+	projectId: "--------------",
+	storageBucket: "--------------.appspot.com",
+	messagingSenderId: "--------------",
+	appId: "-----------------------------------------",
+	measurementId: "-----------"
 
 };
 
